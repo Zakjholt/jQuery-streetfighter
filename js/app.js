efdfd0752636291ad9fad3fd247e9f2c14a0f528
zak <zak.j.holt@gmail.com>
2016-08-13 20:20:28 -0400

    hadouken finished

--- js/app.js
+++ js/app.js
@@ -8,9 +8,10 @@ $(document).ready(function() {
     $('.ryu-still').show();
   })
   .mousedown(function() {
+    playHadouken();
     $('.ryu-ready').hide();
     $('.ryu-throwing').show();
-    $('.hadouken').show().animate(
+    $('.hadouken').finish().show().animate(
       {'left': '1020px'},
       500,
       function() {
@@ -19,7 +20,6 @@ $(document).ready(function() {
       }
     );
     // play hadouken sound
-    // show hadouken and animate it to the right of the screen
   })
   .mouseup(function() {
     $('.ryu-throwing').hide();
@@ -27,3 +27,8 @@ $(document).ready(function() {
     //ryu goes back to his ready position
   });
 });
+function playHadouken() {
+    $('#hadouken-sound')[0].volume = 0.5;
+    $('#hadouken-sound')[0].load();
+    $('#hadouken-sound')[0].play();
+}
