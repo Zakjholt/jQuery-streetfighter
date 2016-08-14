f76ec57a4e09d2f6d3fb9d6e49c6cd1fea07f23c
zak <zak.j.holt@gmail.com>
2016-08-13 22:07:19 -0400

    finished up

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  playBackground();
   $('.ryu').mouseenter(function() {
     $('.ryu-still').hide();
     $('.ryu-ready').show();
@@ -19,16 +20,38 @@ $(document).ready(function() {
         $('.hadouken').css('left', '620px');
       }
     );
-    // play hadouken sound
   })
   .mouseup(function() {
     $('.ryu-throwing').hide();
     $('.ryu-ready').show();
     //ryu goes back to his ready position
   });
+  $(document).keydown(function(x) {
+    if (x.which === 88) {
+      $('.ryu-ready').hide();
+      $('.ryu-cool').show();
+
+    }
+  })
+  .keyup(function() {
+    $('.ryu-cool').hide();
+    $('.ryu-ready').show();
+
+  });
+  $('#musicOn').click(function(){
+    $('#background-music')[0].play();
+  });
+  $('#musicOff').click(function(){
+    $('#background-music')[0].pause();
+  });
 });
 function playHadouken() {
     $('#hadouken-sound')[0].volume = 0.5;
     $('#hadouken-sound')[0].load();
     $('#hadouken-sound')[0].play();
+}
+function playBackground() {
+  $('#background-music')[0].volume = 0.35;
+  $('#background-music')[0].load();
+  $('#background-music')[0].play();
 }
